033a2967edc9050cca834adac70e98829c33a43d
ericwhs <whs_nk@126.com>
2015-10-15 12:41:59 +0800

    //add function secondMoudle ; and  similar pakage of firstMoudle

---
+++ src/first/moudle/FirstMoudle.java
@@ -0,0 +1,7 @@
+package first.moudle;
+
+/**
+ * Created by 80072415 on 2015-10-15.
+ */
+public class FirstMoudle {
+}
---
+++ src/second/moudle/SecondMoudle.java
@@ -0,0 +1,8 @@
+package second.moudle;
+
+
+/**
+ * Created by 80072415 on 2015-10-15.
+ */
+public class SecondMoudle {
+}
